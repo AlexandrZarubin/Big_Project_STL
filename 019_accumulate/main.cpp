d096331a8db89f4b37fbfde016d06049cd30ca6a
Alexander Zarubin <whitesashka@gmail.com>
2024-08-11 13:06:35 +0500

    add 019-020

---
+++ 019_accumulate/main.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<list>
+#include<string>
+
+#include<numeric>
+using namespace std;
+/*
+
+	Библиотека стандарных шаблонов(Stl)
+
+	Алгоритмы
+
+	Cумма элементов
+
+	произведение элементов
+
+	сумма четных элементов
+
+*/
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	vector<int> v = { 2,3,4 };
+	int v1[] = { 2,3,4 };
+	auto result = accumulate(begin(v1),end(v1), 0);//сумма
+	auto result1 = accumulate(v.begin(), v.end(), 0);//сумма
+	auto result2 = accumulate(v.begin(), v.end(), 1, [](int a, int b) {return a * b; });//произведение
+	auto result3 = accumulate(v.begin(), v.end(), 0, [](int a, int b) //сумма четных элеметов
+		{
+		if (b % 2 == 0) 
+		{
+			return a + b;
+		} 
+		else
+		{
+			return a;
+		}
+		
+		});
+	auto result4 = accumulate(next(v.begin()), v.end(), to_string(v[0]), [](string a, int b) {return a+"-"+to_string(b); });
+	cout << "RESULT: " << result << endl;
+	cout << "RESULT1: " << result1 << endl;
+	cout << "RESULT2: " << result2 << endl;
+	cout << "RESULT3: " << result3 << endl;
+	cout << "RESULT3: " << result4 << endl;
+	
+
+
+	return 0;
+}
---
+++ 020_equal_and_mismatch/main.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<list>
+
+using namespace std;
+
+/*
+
+	Библиотека стандарных шаблонов(Stl)
+
+	Алгоритмы
+
+	сравнение две последовательности
+
+	equal и mismatch
+*/
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int arr[] = { 10,3,4,5,7,11 };
+	vector<int> arr2 = { 10,3,4,5,7,11 };
+
+	bool result = equal(begin(arr), end(arr), begin(arr2),end(arr2));
+
+	vector<int> arr3 = { 11,3,4,5,7,10 };
+	vector<int> arr4 = { 10,3,4,5,7,11 };
+	//sort(begin(arr3), end(arr3));
+	//sort(begin(arr4), end(arr4));
+	bool result2= equal(begin(arr3), end(arr3), begin(arr4),end(arr4));
+	cout << result << endl;
+	cout << result2 << endl;
+
+	auto result3 = mismatch(begin(arr), end(arr), begin(arr2), end(arr2));
+
+	if (result3.first == end(arr) && result3.second == end(arr2))
+	{
+		cout << "+" << endl;
+	}
+	else
+	{
+		cout << "-" << endl;
+		cout << "result.first:\t" << *result3.first << "\t result.second:\t" << *result3.second << endl;
+	}
+
+	return 0;
+}
